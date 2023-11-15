ac30642d1737153b43d523211a614a75b5bd11d1
James Titcumb <james@asgrim.com>
2023-11-15 13:42:04 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# 🔥 Arson
+
+**A**sgrim's **R**ust-based J**SON** viewer tool.
+
+This is a GTK3-based UI to view and manipulate JSON.
+
+## Features
+
+ * Minify JSON
+ * Prettify JSON
+
+## Feature ideas
+
+ * Open from a file
+ * Open from a URL
+ * Opening from Nautilus context (e.g. "Open with Arson")
+ * Syntax highlighting
+ * Render JSON as tree
---
+++ src/main.rs
@@ -0,0 +1,143 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Box, Orientation, Toolbar, ToolButton, ScrolledWindow, ShadowType, TextView, Menu, MenuBar, MenuItem, AboutDialog};
+use serde_json::Value;
+
+fn main() {
+    let app = Application::builder()
+        .application_id("com.jamestitcumb.ArsonGtk")
+        .build();
+
+    app.connect_activate(|app| {
+        let win = ApplicationWindow::builder()
+            .application(app)
+            .default_width(800)
+            .default_height(600)
+            .title("Arson JSON")
+            .build();
+
+        let v_box = Box::builder()
+            .visible(true)
+            .orientation(Orientation::Vertical)
+            .build();
+        win.add(&v_box);
+
+        let file_menu = Menu::new();
+        let file_quit_item = MenuItem::builder()
+            .label("Quit")
+            .build();
+        file_menu.append(&file_quit_item);
+
+        let help_menu = Menu::new();
+        let help_about_item = MenuItem::builder()
+            .label("About")
+            .build();
+        help_menu.append(&help_about_item);
+
+        let menu_bar = MenuBar::new();
+        let file_item = MenuItem::builder()
+            .label("File")
+            .submenu(&file_menu)
+            .build();
+        let help_item = MenuItem::builder()
+            .label("Help")
+            .submenu(&help_menu)
+            .build();
+        menu_bar.append(&file_item);
+        menu_bar.append(&help_item);
+        v_box.pack_start(&menu_bar, false, false, 0);
+
+        let toolbar = Toolbar::builder()
+            .visible(true)
+            .expand(false)
+            .build();
+        v_box.add(&toolbar);
+
+        let pretty_button = ToolButton::builder()
+            .visible(true)
+            .label("Pretty")
+            .tooltip_text("Format this JSON to be human-readable")
+            .is_important(true)
+            .use_underline(true)
+            .icon_name("format-indent-more")
+            .build();
+        toolbar.add(&pretty_button);
+
+        let minify_button = ToolButton::builder()
+            .visible(true)
+            .label("Minify")
+            .tooltip_text("Remove extra spaces and newlines to compact space")
+            .is_important(true)
+            .use_underline(true)
+            .icon_name("format-justify-fill")
+            .build();
+        toolbar.add(&minify_button);
+
+        let scrolled_window = ScrolledWindow::builder()
+            .visible(true)
+            .can_focus(true)
+            .shadow_type(ShadowType::In)
+            .expand(true)
+            .build();
+        v_box.add(&scrolled_window);
+
+        let text_view = TextView::builder()
+            .visible(true)
+            .can_focus(true)
+            .monospace(true)
+            .build();
+        scrolled_window.add(&text_view);
+
+        pretty_button.connect_clicked({
+            let text_view = text_view.clone();
+            move |_| {
+                let buffer = text_view.buffer().unwrap();
+                let (start, end) = buffer.bounds();
+                let pretty_json = buffer.text(&start, &end, true).unwrap();
+
+                // @todo handle invalid JSON - currently we crash
+                let v: Value = serde_json::from_str(pretty_json.as_str()).unwrap();
+
+                buffer.set_text(&serde_json::to_string_pretty(&v).unwrap());
+            }
+        });
+
+        minify_button.connect_clicked({
+            let text_view = text_view.clone();
+            move |_| {
+                let buffer = text_view.buffer().unwrap();
+                let (start, end) = buffer.bounds();
+                let ugly_json = buffer.text(&start, &end, true).unwrap();
+
+                // @todo handle invalid JSON - currently we crash
+                let v: Value = serde_json::from_str(ugly_json.as_str()).unwrap();
+
+                buffer.set_text(&serde_json::to_string(&v).unwrap());
+            }
+        });
+
+        file_quit_item.connect_activate({
+            let win = win.clone();
+            move |_| {
+                win.close();
+            }
+        });
+
+        help_about_item.connect_activate({
+            let win = win.clone();
+            move |_| {
+                let p = AboutDialog::new();
+                p.set_website_label(Some("github.com/asgrim/arson"));
+                p.set_website(Some("https://github.com/asgrim/arson"));
+                p.set_authors(&["James Titcumb"]);
+                p.set_title("About Arson");
+                p.set_transient_for(Some(&win));
+                p.set_logo_icon_name(Some("config-users"));
+                p.show_all();
+            }
+        });
+
+        win.show_all();
+    });
+
+    app.run();
+}
